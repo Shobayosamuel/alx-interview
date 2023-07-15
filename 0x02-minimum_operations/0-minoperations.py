5031c7bd8c84c3276ac195ab789f687ebbeb01df
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-15 19:19:59 +0000

    fix error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,12 +8,20 @@ from math import sqrt
 
 
 def minOperations(n):
-    """Return the minimum number of operations"""
-    factors = []
-    for i in range(2, int(sqrt(n) + 1)):
-        if n % i == 0:
-            factors.append(i)
-        n //= i
-    if n > 1:
-        factors.append(n)
-    return sum(factors)
+    """
+    calculates the fewest number of operations needed
+    to result in exactly n H characters in the file
+    """
+    if n < 2:
+        return 0
+    prime_list = []
+    while n % 2 == 0:
+        prime_list.append(2)
+        n /= 2
+    for i in range(3, int(sqrt(n)) + 1, 2):
+        while n % i == 0:
+            prime_list.append(i)
+            n /= i
+    if n > 2:
+        prime_list.append(n)
+    return int(sum(prime_list))
