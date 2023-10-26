485b908088eed66e2f0b5e815ed531183d28303a
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-10-26 15:14:13 +0000

    fix validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,8 +7,37 @@
 
 def validUTF8(data):
     """Return True if data is a valid utf-8 encoding"""
-    new = [val > -128 and val <= 127 for val in data]
-    if False in new:
-        return False
-    else:
-        return True
+    expected_continuation_bytes = 0
+
+    # Iterate through the list of integers
+    for byte in data:
+        # Convert the byte to an 8-bit binary string
+        binary_byte = format(byte, '08b')
+
+        # If we are not expecting continuation bytes, check the prefix
+        if expected_continuation_bytes == 0:
+            if binary_byte[0] == '0':
+                # This is a single-byte character
+                continue
+            elif binary_byte[:3] == '110':
+                # This is the start of a 2-byte character
+                expected_continuation_bytes = 1
+            elif binary_byte[:4] == '1110':
+                # This is the start of a 3-byte character
+                expected_continuation_bytes = 2
+            elif binary_byte[:5] == '11110':
+                # This is the start of a 4-byte character
+                expected_continuation_bytes = 3
+            else:
+                # Invalid prefix
+                return False
+        else:
+            # If this byte is a continuation byte (starts with '10')
+            if binary_byte[:2] == '10':
+                expected_continuation_bytes -= 1
+            else:
+                # Invalid continuation byte
+                return False
+
+    # If there are no unexpected continuation bytes left, it's a valid UTF-8 encoding
+    return expected_continuation_bytes == 0
