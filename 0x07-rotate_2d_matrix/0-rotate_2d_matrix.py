2622e3fe866c8ac5f6e2e16999eaa814449e4b7d
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-14 19:43:09 +0000

    Rotate 2D array

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    __description__:
+        Rotate a 2D Array
+"""
+
+def rotate_2d_matrix(matrix):
+    """Function to reverse a 2D array"""
+    num = len(matrix)
+    for i in range(num):
+        for j in range(i, num):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+    for row in matrix:
+        row.reverse()
